63f25c1e50e4babb003ef5300a722d45f2e99a62
kerry.cho <cdh0012@inveniacorp.com>
2019-10-25 13:57:59 +0900

    Add Install Scripts 1. Add silent install feature  - Install OpenCV 4.1.1  - Install Libtorch 1.3

---
+++ Scripts/ps/_operations.ps1
@@ -0,0 +1,16 @@
+﻿Function Set-OperationsInfo()
+{
+    @(
+        @{Name = "OpenCV-4.1.1-vc14-vc15"; ShortName = "CV-411"; Info = "Install OpenCV-4.1.1-vc14-vc15";
+            Verification = @( @{Function = "VerifyDirectory"; Path = "$OpenCVBasePath"; } );
+            Download = @( );
+            Action = @( @{Function = "InstallExe"; Command = "$localCache\opencv-4.1.0-vc14_vc15.exe"; Param = "-y -oD:\Local2"; Message="Installing opencv ....";}); 
+        }        
+    )
+    ,
+    @{Name = "LibTorch-1.3"; ShortName = "TORCH-1.3"; Info = "Install Libtorch";
+        Verification = @( @{Function = "VerifyDirectory"; Path = "$OpenCVBasePath"; } );
+        Download = @( );
+        Action = @( @{Function = "ExtractAllFromZip"; zipFileName = "$localCache\libtorch-win-shared-with-deps-1.3.0.zip"; destinationFolder = "D:\Local2"; } );
+   }
+}
---
+++ Scripts/ps/install.ps1
@@ -0,0 +1,54 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$false)] [string] $OpenCVBasePath = "D:\local2\opencv",
+    [Parameter(Mandatory=$false)] [switch] $Execute = $true,
+    [Parameter(Mandatory=$false)] [switch] $NoConfirm)
+
+Set-StrictMode -Version latest
+
+Import-Module Download -ErrorAction Stop
+
+$MyDir = Split-Path $MyInvocation.MyCommand.Definition
+$ymlDir = Split-Path $MyDir
+
+$roboCopyCmd    = "C:\Windows\System32\robocopy.exe"
+$localCache     = "$MyDir\InstallCache"
+
+. "$MyDir\_operations"
+. "$MyDir\_verify"
+. "$MyDir\_download"
+. "$MyDir\_info"
+. "$MyDir\_action"
+
+Function main
+{
+    try {
+        if (-not (DisplayStart -NoConfirm $NoConfirm)) {
+            Write-Host  
+            Write-Host " ... Quitting ... "
+            Write-Host
+            return
+        }
+        
+        $operations = Set-OperationsInfo 
+
+        $Script:operationList  = @()
+        $Script:WinProduct = $null
+        if (VerifyOperations -NoConfirm $NoConfirm) 
+        {
+            DownloadOperations
+            
+            ActionOperations
+
+            DisplayEnd
+        }
+    }
+    catch {
+        Write-Host `nFatal error during script execution!`n($Error[0]).Exception`n
+        exit 1
+    }
+}
+
+main
+
+exit 0
